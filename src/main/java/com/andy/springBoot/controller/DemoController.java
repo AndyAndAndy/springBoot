19891860d60d3c8d8b0a4ff4dbcc919ebdbdc1bf
Andy.chen <chenhongqiao.jkinfo.com.cn>
2017-02-14 17:05:34 +0800

    feat:添加springBoot入门项目

---
+++ src/main/java/com/andy/springBoot/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.andy.springBoot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	
+	public String demo() {
+        return "springBoot Controller demo!";
+    }
+
+}
---
+++ src/test/java/com/andy/springBoot/AppTest.java
@@ -0,0 +1,7 @@
+package com.andy.springBoot;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+}
